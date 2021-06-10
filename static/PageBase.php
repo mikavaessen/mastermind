a7598e0139508cbd4458de3144a7a1438c0a9b95
mikavaessen <85626985+mikavaessen@users.noreply.github.com>
2021-06-10 14:18:27 +0200

    Worked on login page

---
+++ static/PageBase.php
@@ -0,0 +1,11 @@
+{% extends 'base.php' %}
+
+{% block head %}
+<link rel="stylesheet" href="{{ url_for('static', filename='css/index.css')}}">
+<title>MM | [[TITLE]]</title>
+{% endblock %}
+{% block body %}
+<main>
+
+</main>
+{% endblock %}
