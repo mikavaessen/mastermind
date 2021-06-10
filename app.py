53cec7ec3e30ddf83a444d64be44b14f8e1bcf99
mikavaessen <85626985+mikavaessen@users.noreply.github.com>
2021-06-10 17:59:08 +0200

    Update 10 06 17:58
    Worked on game page
    Partiallially implemented Gameplay class

--- app.py
+++ app.py
@@ -2,20 +2,31 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import request
+from backend import GamePlay
 
 names = ['Select nickname']
-
 app = Flask(__name__)
+Game:GamePlay
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        #Go to game settings
         if request.form['Msg'] == 'login':
             return render_template('Login.php', names=names)
+        #Show statistics
         elif request.form['Msg'] == 'stats':
             return render_template('Statistics.php')
         elif request.form['Msg'] == 'StartGame':
-            #Start new game
+            #Start new game after 
+            colourAmount = request.form['colours']
+            tryAmount = request.form['tries']
+            gameMode = ''
+            if request.form.get('hardmode'):
+                gameMode = 'hard'
+            else:
+                gameMode = 'normal'
+            #Game = GamePlay(colourAmount, tryAmount, gameMode)
 
             return render_template('Game.php', inserts=None, feedback=None)
         elif request.form['Msg'] == 'CheckResult':
--- templates/Game.php
+++ templates/Game.php
@@ -7,6 +7,44 @@
 {% block body %}
 <main>
     <h3><u>Game window</u></h3>
+    <div id="gameWindow">
+        <div id="Board">
+            {% if inserts == NULL %}
+            <div id="col0">
+                {% for in range(0, tries) %}
+                <span class="empty" id="row{{i}}"></span>
+                {% endfor %}
+            </div>
+            <div id="col1">
+                {% for in range(0, tries) %}
+                <span class="empty" id="row{{i}}"></span>
+                {% endfor %}
+            </div>
+            <div id="col2">
+                {% for in range(0, tries) %}
+                <span class="empty" id="row{{i}}"></span>
+                {% endfor %}
+            </div>
+            <div id="col3">
+                {% for in range(0, tries) %}
+                <span class="empty" id="row{{i}}"></span>
+                {% endfor %}
+            </div>
+            <div id="colFb">
+
+            </div>
+            {% for i in range(0, tries) %}
+            <span class="empty" id="row{{i}} col0"></span>
+            <span class="empty" id="row{{i}} col1"></span>
+            <span class="empty" id="row{{i}} col2"></span>
+            <span class="empty" id="row{{i}} col3"></span>
+        </div>
+    </div>
+    <div id="controls">
+
+    </div>
+    {% for i in range(0, tries) %}
+
         <!--
             Veld van 4x10 met per rij 4 plekken voor feedback pins
         -->
