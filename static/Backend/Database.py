e8c04f64434f1b42cbf99b536b48abc9da49f79b
mikavaessen <85626985+mikavaessen@users.noreply.github.com>
2021-06-11 20:27:58 +0200

    8r9gs8ehg90

---
+++ static/Backend/Database.py
@@ -0,0 +1,39 @@
+import pyodbc
+from datetime import datetime
+
+class Database():
+    def __init__(self):
+        self.conStr = ''
+        self.conn = pyodbc.connect(self.conStr)
+
+    def getNames(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT DISTINCT name FROM data")
+        data = list(str)
+        for row in cursor:
+            data.append(row[0])
+        return data
+
+    def getPlayerData(self, name:str):
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT * FROM data WHERE name = {name}")
+        return cursor
+
+    def addGame(self, name:str, tries:int):
+        if self.checkForbidden(name):
+            return
+        cursor = self.conn.cursor()
+        now = datetime.now().strftime("%c")
+        cursor.execute(f"INSERT INTO data VALUES (NEWID(), {name}, {now}, {tries})")
+        self.conn.commit()
+
+    def checkForbidden(self, string):
+        forbiiden = "';," + '"'
+        for i in string:
+            for j in forbiiden:
+                if i == j:
+                    return True
+        return False
+
+    def close(self):
+        del self.conn
--- static/Backend/game.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-class Game():
-    def __init__(self, guesses):
-        self.guesses = guesses
-        self.time = datetime.now()
